c237d1b7c7cbf31a699257b67958f6f9ec324d5a
tanith-tr <tanith_tr@yahoo.co.uk>
2015-03-07 18:57:39 +0200

    Add the PNG files and R code files

---
+++ plot2.R
@@ -0,0 +1,37 @@
+# Exploratory Data Analysis Course Project 1 - Plot 2
+
+# This assignment uses “Individual household electric power consumption Data Set” data from 
+# the UC Irvine Machine Learning Repository.
+
+# The dataset can be downloaded from 
+# https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# The unzipped data file must be placed in the working directory.
+
+# We will only be using data from the dates 2007-02-01 and 2007-02-02. 
+
+
+# Set locale time to English if your local language is different
+Sys.setlocale("LC_TIME", "English") 
+
+# Read the data 
+dataAll <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", dec = ".", 
+                      stringsAsFactors = FALSE, na.strings = "?", 
+                      colClasses = c(rep("character",2), rep("numeric",7)))
+
+# Subset the data of the dates 2007-02-01 and 2007-02-02 and clear dataAll from workspace
+dataSub <- subset(dataAll, Date == c("1/2/2007") | Date == c("2/2/2007"))
+rm(dataAll)
+
+# Create DateTime variable
+dataSub$DateTime <- paste(dataSub$Date, dataSub$Time, sep = " ")
+
+#Convert date and time variables
+dataSub$Date <- as.Date(dataSub$Date, "%d/%m/%Y")
+dataSub$DateTime <- strptime(dataSub$DateTime, "%d/%m/%Y %H:%M:%S")
+
+# Construct the plot and save it to a PNG file with a width of 480 pixels and a height of 480 pixels
+png(filename = "plot2.png", width = 480, height = 480, units = "px", bg = "white")
+plot(dataSub$DateTime, dataSub$Global_active_power, xlab ="", ylab = "Global Active Power (kilowatts)", type ="l")
+dev.off()
+
+
